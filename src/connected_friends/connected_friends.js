fa8f101c619c59e89ab4ea2d58fb7b5608f9d713
Tina <tinazhang616@gmail.com>
2023-07-10 10:53:02 -0700

    add-comments

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,5 +1,29 @@
 const Queue = require("../lib/queue");
+/* If graph is empty (has no keys), return false.
 
+If startUser is equal to endUser, return true.
+
+Initialize a new array, enqueued, that contains startUser.
+
+Initialize a new empty queue named discovered.
+
+Enqueue startUser.
+
+While discovered isn't empty, do the following:
+
+Dequeue a value from discovered and name it user.
+
+For each friend followedUser in graph[user], do the following:
+
+If followedUser is equal to endUser, return true.
+
+If enqueued doesn't include followedUser, do the following:
+
+Add followedUser to enqueued.
+
+Enqueue friend to discovered.
+
+Return false. */
 const connected = (G, s, r) => {
     if(Object.keys(G).length===0){
         return false
